b8d066ed8822ce2b27adff0ca8ca7c5a3481a8d4
uliarce52-creator <uliarce52@gmail.com>
2026-06-25 09:25:19 -0300

    Refactor menu and task processing logic

--- funciones.py
+++ funciones.py
@@ -1,47 +1,49 @@
 # FUNCIONES 
 
 def menu():
+	print()
+	print(" ---- MENU ----")
+	print()
 	print(f"1. Agrega Una Tarea a la Lista")
-	print(f"2. Muestre Las tareas")
-	print(f"3. Marque una tarea completada")
-	print(f"4. Eliminar una tarea de la lista")
-	print(f"5. Salir")
+	print()
+	print(f"2. Marque una tarea completada")
+	print()
+	print(f"3. Eliminar una tarea de la lista")
+	print()
+	print(f"4. Salir")
+	print()
 	eleccion = input(f"Elija una opcion: ")
 	return eleccion
 	
 def procesar(eleccion,lista):
 	
 	if eleccion == "1":
-		agregar = input("Escriba la tarea que quiere agregar: ")
-		lista.append(agregar)
+		agregar = input("Escriba las tareas que quiere agregar(para salir ingrese FIN): ")
+		
+		while agregar != "FIN":
+			lista.append(agregar)
+			
+			
+			agregar = input("Escriba las tareas que quiere agregar(para salir ingrese FIN): ")
+			
 		
 	elif eleccion == "2":
-			
-		if len(lista) == 0:
-			print("\nNo hay tareas pendientes. 🎉\n")
-				
-		else:
-			print("\n--- TAREAS ---")
-					
-			for indice, tareas in enumerate(lista):
-				print(f"{indice + 1}. {tareas}")
-			
-	elif eleccion == "3":
+		
 		completo = int(input("Ingrese el numero de la tarea completada: "))
 		if 1 <= completo <= len(lista):
-			lista[completo - 1] += " (COMPLETADO)"
+			lista[completo -1] += " (!!COMPLETADO!!)"
 			print("¡Tarea marcada como completada!")
 		else:
 			print("Número de tarea no válido.")
 	
-	elif eleccion == 4:
+	elif eleccion == "3":
 		eliminar = int(input("Ingrese el numero de la tarea a eliminar: "))
 		if 1 <= eliminar <= len(lista):
 			
-			print(f"Va a eliminar: {lista[eliminar - 1]}")
+			print(f"Va a eliminar: {lista[eliminar-1]}")
 			
-			lista.pop(eliminar - 1)
+			lista.pop(eliminar)
 			
 			print("¡Tarea eliminada!")
-		else:
-			print("Número de tarea no válido.")
+	else:
+		print("Número de tarea no válido.")
