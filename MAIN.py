551b21fc017d787f6c607d6af1cc6716ace79e78
uliarce52-creator <uliarce52@gmail.com>
2026-06-25 09:24:21 -0300

    Change loop condition from '5' to '4'

--- MAIN.py
+++ MAIN.py
@@ -5,10 +5,26 @@ from funciones import *
 tareas = []
 eleccion = ""
 
-while eleccion != "5":
+while eleccion != "4":
+	
+	if len(tareas) == 0:
+		print()
+		print(f"La lista esta vacia")
+		print()
+	else:
+		print()
+		print(f"Su lista de tareas es la siguiente")
+		print()
+		for i,p in enumerate(tareas):
+			print(f"{i} : {p}")
+		print()
+		print()
+		
+		
 	eleccion = menu()
 	
-	if eleccion != "5":
+	if eleccion != "4":
 		procesar(eleccion,tareas)
 		
 print("Adios!")
+
